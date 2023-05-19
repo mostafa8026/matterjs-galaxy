a996503cc88161140869a9c53ad1d746cb1a400f
Mostafa <mostafa8026@gmail.com>
2023-05-19 17:52:43 +0330

    feat: make the project compatible with matterjs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,54 @@
 import "./style.scss";
 
-import * as _ from 'lodash';
+import { Bodies, Composite, Engine, Mouse, MouseConstraint, Render, Runner } from 'matter-js';
 
-console.log("Hello World From Console!");
+const engine = Engine.create();
+const render = Render.create({
+  element: document.body,
+  engine: engine
+});
 
-function component() {
-    const element = document.createElement('div');
+const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+/**
+ * Creating 100 random boxes WITH RANDOM MESS and add them to the world
+ */
+Composite.add(engine.world, [ground]);
 
-    return element;
-}
+Render.run(render);
+const runner = Runner.create();
+Runner.run(runner, engine);
 
-document.body.appendChild(component());
+/**
+ * Creating a new box everytime user click on a button
+ *   creating the button
+ *   adding event listener
+ */
+const button = document.createElement("button");
+button.innerText = "Add box";
+document.body.appendChild(button);
+button.addEventListener("click", () => {
+  /** Random box in random position with random width, height */
+  const box = Bodies.rectangle(
+    Math.random() * 800,
+    Math.random() * 600,
+    Math.random() * 200,
+    Math.random() * 200
+  );
+  Composite.add(engine.world, [box]);
+});
+
+/**
+ * Make it possible to drag matterjs objects
+ */
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false
+    }
+  }
+});
+Composite.add(engine.world, mouseConstraint);
